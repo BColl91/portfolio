ae9b137d1a6e66929aee33761b81eaf94c8b4652
Rebecca Collins <rebeccacollins1007@gmail.com>
2024-07-29 17:32:06 +0100

    notes added

---
+++ Script.js
@@ -0,0 +1,3 @@
+
+
+//in portfolio add highlight links or mouse option? ADD DISCLAIMER-Loosely designed in Figma and coded in Visual Studio Code by yours truly. Built with Next.js and Tailwind CSS, deployed with Vercel. All text is set in the Inter typeface.
